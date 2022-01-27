4e90001bf0acbee1786a72324be55a2f9274ebcd
DevJohn <johnng87@protonmail.com>
2022-01-27 11:02:18 +0800

    Testing github action

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h2>Select Country list with default selection</h2>
+      <h3>Testing Github Action with Github Pages</h3>
       <div className="select-country">
         <CountrySelector />
       </div>
